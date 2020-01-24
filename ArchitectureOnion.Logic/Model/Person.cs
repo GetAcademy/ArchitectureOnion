9ac85d6b5d83a401e245a7bf9138aaa870d67466
GET Academy <terje@kolderup.net>
2020-01-24 15:17:36 +0100

    x

--- ArchitectureOnion.Logic/Model/Person.cs
+++ ArchitectureOnion.Logic/Model/Person.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace ArchitectureNTier.Logic.Model
+namespace ArchitectureOnion.Logic.Model
 {
     public class Person
     {
--- ArchitectureOnion.Logic/PersonService.cs
+++ ArchitectureOnion.Logic/PersonService.cs
@@ -1,39 +1,33 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using ArchitectureOnion.Logic.Interface;
 using ArchitectureOnion.Logic.Model;
 
-namespace ArchitectureNTier.Logic
+namespace ArchitectureOnion.Logic
 {
     public class PersonService
     {
+        private readonly IPersonRepository _personRepository;
+
+        public PersonService(IPersonRepository personRepository)
+        {
+            _personRepository = personRepository;
+        }
+
         public async Task<Person> GetPerson(int id)
         {
-            var connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=ArchitectureDemo;Integrated Security=True;";
-            var repo = new PersonRepository(connStr);
-            var dbPerson = await repo.ReadOneById(id);
-            var person = LogicPersonFromDbPerson(dbPerson);
-            person.Father = await ReadParent(dbPerson.Father, repo);
-            person.Mother = await ReadParent(dbPerson.Mother, repo);
-            var dbChildren = await repo.ReadChildren(id);
-            person.Children = dbChildren.Select(LogicPersonFromDbPerson).ToArray();
-            return person;
-        }
-
-        private static async Task<Person> ReadParent(int? parentId, PersonRepository repo)
-        {
-            if (parentId == null) return null;
-            var dbParent = await repo.ReadOneById(parentId.Value);
-            return LogicPersonFromDbPerson(dbParent);
-        }
-
-        private static Person LogicPersonFromDbPerson(Data.Model.Person dbPerson)
-        {
-            return new Person
+            var person = await _personRepository.ReadOneById(id);
+            if (person.Father != null)
             {
-                FirstName = dbPerson.FirstName,
-                LastName = dbPerson.LastName,
-                BirthDate = dbPerson.BirthDate,
-                DeathDate = dbPerson.DeathDate
-            };
+                person.Father = await _personRepository.ReadOneById(person.Father.Id);
+            }
+            if (person.Mother != null)
+            {
+                person.Mother = await _personRepository.ReadOneById(person.Mother.Id);
+            }
+            var children = await _personRepository.ReadChildren(id);
+            person.Children = children.ToArray();
+            return person;
         }
     }
 }
